e8148f94cc6f7245eb97ff301dba038e7d1b7566
Luyao Wang <lywlywly@gmail.com>
2024-02-25 09:33:31 -0500

    client continuous post

--- hw2/client.py
+++ hw2/client.py
@@ -2,9 +2,29 @@ import argparse
 import json
 
 import requests
+import logging
+import time
+import os
 
+log_filename = 'log.log'
+if os.path.exists(log_filename):
+    os.remove(log_filename)
 
-def run(tracks, ip, port):
+logging.basicConfig(
+    filename=log_filename,
+    level=logging.DEBUG,
+    format="%(asctime)s | %(levelname)s | %(message)s",
+)
+
+def run(tracks, ip, port, continuous):
+    if continuous:
+        while True:
+            _run(tracks, ip, port)
+            time.sleep(0.1)
+    else:
+        _run(tracks, ip, port)
+
+def _run(tracks, ip, port):
     payload = {"songs": tracks}
     json_payload = json.dumps(payload)
 
@@ -18,20 +38,26 @@ def run(tracks, ip, port):
 
     response_json = json.loads(response.text)
     print(f"Code version: {response_json['version']}")
+    logging.info(f"Code version: {response_json['version']}")
     print(f"Model time: {response_json['model_date']}")
+    logging.info(f"Model time: {response_json['model_date']}")
     print(f"Dataset: {response_json['dataset']}")
+    logging.info(f"Dataset: {response_json['dataset']}")
     print(f"Recommended songs: {response_json['songs']}")
+    logging.info(f"Recommended songs: {response_json['songs']}")
 
 def main():
     parser = argparse.ArgumentParser(description="Playlist recommendation.")
     parser.add_argument("-H", "--host", required=True, help="Host IP address.")
     parser.add_argument("-p", "--port", required=True, help="Port number.")
     parser.add_argument("-i", "--input", required=True, nargs="+", help="Input tracks.")
+    parser.add_argument("-c", "--continuous", action='store_true', default=False, help="Continuous send requests, save result in responses.txt.")
     args = parser.parse_args()
     tracks = args.input
     ip = args.host
     port = args.port
-    run(tracks, ip, port)
+    continuous = args.continuous
+    run(tracks, ip, port, continuous)
 
 
 main() if __name__ == "__main__" else None
